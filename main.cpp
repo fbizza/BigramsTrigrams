91edbf8b7e5a66d9c9a3224ddc047cef5f8fda36
bizza <bizzafrancesco@mail.com>
2023-01-05 16:59:02 +0100

    Add basic clean_txt function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <cctype>
 
 using namespace std;
+
+
+int clean_txt() {
+    string filename = "../input.txt";
+    ifstream input(filename);
+    if (!input.is_open()) {
+        cerr << "Error opening input file: " << filename << endl;
+        return 1;
+    }
+    string word;
+    ofstream output("../output.txt");
+    if (!output.is_open()) {
+        cerr << "Error opening output file: output.txt" << endl;
+        return 1;
+    }
+    while (input >> word) {
+        string clean_word;
+        for (char c : word) {
+            if (!ispunct(c)) {
+                clean_word += tolower(c);
+            }
+        }
+        if (!clean_word.empty()) {
+            output << clean_word << endl;
+        }
+    }
+    input.close();
+    output.close();
+}
 
 vector<string> extract_bigrams(const string& str) {
     vector<string> bigrams;
@@ -14,6 +45,7 @@ vector<string> extract_bigrams(const string& str) {
 
 int main()
 {
+    clean_txt();
     string str = "Ciao, mi chiamo Francesco";
     vector<string> bigrams = extract_bigrams(str);
     for (const string& bg : bigrams) {
